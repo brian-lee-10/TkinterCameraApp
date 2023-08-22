d491eb7abafe583adccd6b32e1464d35b6dbb368
brian-lee-10 <brianlee891@gmail.com>
2023-08-22 09:35:30 +0800

    Modified rotate.py

--- rotate.py
+++ rotate.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
 
+# Initialize video capture
 cam = cv2.VideoCapture(0)
 
+# Set frame rate to 30
 cam.set(cv2.CAP_PROP_FPS, 30)
+
+# Declare necessary variables
 key_num = 0
 mirror = False
 
 while True:
+	# Check to see if the camera is open
 	if not cam.isOpened():
 		print("Error opening camera")
 		break
@@ -15,24 +20,31 @@ while True:
 	# read frame
 	ret, frame = cam.read()
 
+	# Flip horizontally if mirror is True
 	if mirror:
 		frame = cv2.flip(frame, 1)
 
-	#frame = cv2.rotate(frame, key_num)
+	# Rotate frame by key_num increments
 	frame = np.rot90(frame, key_num)
+	#frame = cv2.rotate(frame, key_num)
 	
+	# Display camera frame
 	cv2.imshow('Video', frame)
 	
+	# Increment rotation angle on 'r' keypress
 	if cv2.waitKey(1) == ord('r'):
 		key_num=key_num+1
 		if key_num >= 4:
 			key_num = 0
 			
+	# Toggle mirroring on 'm' keypress
 	if cv2.waitKey(1) == ord('m'):
 		mirror = not mirror
 
+	# Break loop on 'q' keypress
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
+# Release camera and destroy opened windows
 cam.release()
 cv2.destroyAllWindows()
