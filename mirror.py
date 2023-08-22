d2664e628ba814cf323a975fee9849efa7cc458f
brian-lee-10 <brianlee891@gmail.com>
2023-08-22 15:21:53 +0800

    Added mirror.py

---
+++ mirror.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+cam = cv2.VideoCapture(0)
+
+cam.set(cv2.CAP_PROP_FPS, 30)
+mirror = False
+
+while True:
+	if not cam.isOpened():
+		print("Error opening camera")
+		break
+	
+	# read frame
+	ret, frame = cam.read()
+
+	if mirror:
+		frame = cv2.flip(frame, 1)
+	
+	cv2.imshow('Video', frame)
+
+	if cv2.waitKey(1) == ord('m'):
+		mirror = not mirror
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cam.release()
+cv2.destroyAllWindows()
